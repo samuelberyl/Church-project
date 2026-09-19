3c00ce82ee9b05ddcbff3e431ee7639bd59df8a1
samueberyl <samueberyl@outlook.com>
2026-09-19 13:21:41 +0300

    this is my javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+const module=document.getElementById('prayer module')
+const form=document.getElementById('prayer form')
+function openmodole(){if(!)return
+    modole.style.display="flex"
+    document.body.classList.add(modole-open)
+    const firstfield=document.getElementById("name")
+    if(firstfield){
+        setTimeout() =>firstfield.focus(
+            {},50
+
+        )
+    }
+}
